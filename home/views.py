e7ed508e5ed4af0646e5056901406c6314418ea3
Saurav <sp.gharti@gmail.com>
2018-03-30 21:55:06 +0000

    add package djangocms-twitter

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'djangocms-twitter', 'url': 'http://pypi.python.org/pypi/djangocms-twitter/0.0.5'},
 	{'name':'django-oauth-twitter', 'url': 'http://pypi.python.org/pypi/django-oauth-twitter/1.11'},
 	{'name':'django-twitter-api', 'url': 'http://pypi.python.org/pypi/django-twitter-api/0.2.10'},
 	{'name':'django-twitter-bootstrap', 'url': 'http://pypi.python.org/pypi/django-twitter-bootstrap/3.3.0'},
